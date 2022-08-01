39cbbab733efed11af45fe006be6a493ee71dbc6
shoval-ba <shoval-ba@cyber4s.dev>
2022-08-01 16:02:05 +0300

    refactor

--- pokedex-project/src/client/favorite.ts
+++ pokedex-project/src/client/favorite.ts
@@ -1,7 +1,7 @@
-import { pokemons, favoriteList } from './app';
-import { pokemonInArray } from './search';
+import { favoriteList } from './app';
+import { Pokemon } from './pokemonComp';
 
-export async function addToFavorite(event:any){
+export async function addToFavorite(event:any , pokemons:Pokemon[]){
   const id = event.target.id;
   for (const pokemon of pokemons){
     if (pokemon.id == id){
@@ -46,47 +46,4 @@ export async function addToFavorite(event:any){
   }
 }
 
-export async function addToFavoriteAfterSearch(event:any){
-  const id = event.target.id;
-  for (const pokemon of pokemonInArray){
-    if (pokemon.id == id){
-      const items = document.querySelectorAll('.backgroundImg') ;
-      for (let i = 0; i < items.length; i++) {
-        const itemDiv = items[i];
-        if (itemDiv.id == id){
-          const starImages = document.querySelectorAll('.starImage');
-          for (let j=0;j <starImages.length;j++){
-            const starImage = starImages[j] as HTMLElement;
-            if (starImage!.id == id){
-              if (starImage!.style.opacity == '1'){
-                  starImage!.style.opacity = '0';
-                  const index:number = favoriteList.indexOf(pokemon);
-                  if (index! === undefined) return;
-                  favoriteList.splice(index, 1);
-                  const options ={
-                    method: 'POST',
-                    headers: {
-                      'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(pokemon)
-                  };
-                  fetch('deleteFavorite', options);
-              } else {
-                  starImage!.style.opacity = '1';
-                  favoriteList.push(pokemon);
-                  const options ={
-                    method: 'POST',
-                    headers: {
-                      'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(pokemon)
-                  };
-                  fetch('addToFavorite', options);
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
+
